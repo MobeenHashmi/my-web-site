e93083938ef8a7d07f386cc3b0b37201289585f7
MobeenHashmi <mobeenhasan00038@gmail.com>
2023-02-01 18:29:11 +0500

    4th change

--- leapyear.js
+++ leapyear.js
@@ -1,13 +1,18 @@
-function isLeapYear(year) {
-    if (year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0)) {
-      return true;
-    } else {
-      return false;
-    }
+const dashTwixt2Evens = (num) =>{
+    const numStr = num.toString();
+    let result = '';
+
+  for (let i = 0; i < numStr.length - 1; i++) {
+    const currentDigit = parseInt(numStr[i]);
+    const nextDigit = parseInt(numStr[i + 1]);
+
+    result += currentDigit;
+    if (currentDigit % 2 === 0 && nextDigit % 2 === 0) result += '-';
   }
-  
-  const year = 2020;
-  const result = isLeapYear(year) ? `${year} is a leap year.` : `${year} is not a leap year.`;
-  
-  console.log(result);
-  
+
+  result += numStr[numStr.length - 1];
+  return result;
+}
+
+let number = 2465367864 ;
+console.log(dashTwixt2Evens(number));

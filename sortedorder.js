80fedcb4c7ccedbae58ba18450d864fd21d9de19
MobeenHashmi <mobeenhasan00038@gmail.com>
2023-02-02 10:46:01 +0500

    8th commit

--- sortedorder.js
+++
@@ -1,9 +0,0 @@
-function sortArrayAscending(array) {
-    return array.sort((a, b) => a - b);
-  }
-  
-  const numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
-  const sortedNumbers = sortArrayAscending(numbers);
-  
-  console.log("Numbers in ascending order:", sortedNumbers);
-  
